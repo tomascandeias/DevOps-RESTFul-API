d966332160ceed548490f0407672a6ef2a107b75
tomascandeias <tomaslcandeias@outlook.com>
2024-02-15 16:29:16 +0100

    revert student service update 2

--- student-app/swagger_server/controllers/default_controller.py
+++ student-app/swagger_server/controllers/default_controller.py
@@ -16,12 +16,11 @@ def add_student(body=None):  # noqa: E501
     :type body: dict | bytes
 
     :rtype: float
-        if connexion.request.is_json:
+    """
+    if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
         return add(body)
     return 500, 'error'
-    """
-    return add(body)
 
 
 def delete_student(student_id):  # noqa: E501
