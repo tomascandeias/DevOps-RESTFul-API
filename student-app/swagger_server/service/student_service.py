e200bf7874aa67b8c3c147d3e3323aed91b8ccf6
tomascandeias <tomaslcandeias@outlook.com>
2024-02-16 13:43:29 +0100

    POST student returns student object

--- student-app/swagger_server/service/student_service.py
+++ student-app/swagger_server/service/student_service.py
@@ -35,7 +35,7 @@ def add(student=None):
         print("Adding student...")
         insert_result = student_collection.insert_one(json.loads(json.dumps(student, cls=JSONEncoder)))
         idx += 1
-        return f"{student.student_id}"
+        return student
 
 
 def get_by_id(student_id=None, subject=None):
